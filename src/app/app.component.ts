b60696483c0dfcbb197a3cef3518a64ab1d7c2be
jeneser <jeneserwang@gmail.com>
2017-06-27 23:50:24 +0800

    Platform separately: Each platform does not affect each other

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,6 +32,13 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       // this.statusBar.styleDefault();
+
+      // Transparent status bar for android
+      // #AARRGGBB where AA is an alpha value
+      if (this.platform.is('android')) {
+        this.statusBar.backgroundColorByHexString("#33000000");
+      }
+
       this.splashScreen.hide();
     });
   }
