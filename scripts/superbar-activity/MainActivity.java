ddc99ea4f62d2c45e3f52975e0f7c45bc7cd7dde
Paulo Cézar Santana <pcsantana2909@gmail.com>
2019-01-07 15:38:01 -0200

    Fixed issue with input behind keyboard

--- scripts/superbar-activity/MainActivity.java
+++ scripts/superbar-activity/MainActivity.java
@@ -19,11 +19,16 @@
 
 package wang.jeneser.superbar;
 
+import android.graphics.Rect;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.view.OnApplyWindowInsetsListener;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.WindowInsetsCompat;
 import android.view.View;
+import android.view.ViewTreeObserver;
 
-import org.apache.cordova.*;
+import org.apache.cordova.CordovaActivity;
 
 public class MainActivity extends CordovaActivity
 {
@@ -32,10 +37,15 @@ public class MainActivity extends CordovaActivity
     {
         super.onCreate(savedInstanceState);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
             getWindow().getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
                             View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+
+            final View contentView = findViewById(android.R.id.content);
+
+            enableKeyboardHelper(contentView);
         }
 
         // enable Cordova apps to be started in the background
@@ -46,5 +56,55 @@ public class MainActivity extends CordovaActivity
 
         // Set by <content src="index.html" /> in config.xml
         loadUrl(launchUrl);
+    }
+
+    /**
+     * Based on:
+     * https://stackoverflow.com/a/52622795/6031927
+     * https://github.com/mikepenz/MaterialDrawer/blob/aa9136fb4f5b3a80460fe5f47213985026d20c88/library/src/main/java/com/mikepenz/materialdrawer/util/KeyboardUtil.java
+     */
+    private void enableKeyboardHelper(final View contentView) {
+        if(contentView != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+                ViewCompat.setOnApplyWindowInsetsListener(contentView , new OnApplyWindowInsetsListener() {
+                    @Override
+                    public WindowInsetsCompat onApplyWindowInsets(View v, WindowInsetsCompat insets) {
+                        v.setPadding(0, 0, 0, insets.getSystemWindowInsetBottom());
+                        return insets;
+                    }
+                });
+
+            } else {
+                getWindow().getDecorView().getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                    @Override
+                    public void onGlobalLayout() {
+                        Rect r = new Rect();
+                        //r will be populated with the coordinates of your view that area still visible.
+                        getWindow().getDecorView().getWindowVisibleDisplayFrame(r);
+
+                        //get screen height and calculate the difference with the useable area from the r
+                        int height = getWindow().getDecorView().getContext().getResources().getDisplayMetrics().heightPixels;
+                        int diff = height - r.bottom;
+
+                        //if it could be a keyboard add the padding to the view
+                        if (diff != 0) {
+                            // if the use-able screen height differs from the total screen height we assume that it shows a keyboard now
+                            //check if the padding is 0 (if yes set the padding for the keyboard)
+                            if (contentView.getPaddingBottom() != diff) {
+                                //set the padding of the contentView for the keyboard
+                                contentView.setPadding(0, 0, 0, diff);
+                            }
+                        } else {
+                            //check if the padding is != 0 (if yes reset the padding)
+                            if (contentView.getPaddingBottom() != 0) {
+                                //reset the padding of the contentView
+                                contentView.setPadding(0, 0, 0, 0);
+                            }
+                        }
+                    }
+                });
+            }
+        }
     }
 }
